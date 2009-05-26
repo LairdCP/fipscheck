c605bdb7fd12ba2c095d6ba21ef6151e95d6711a
Tomas Mraz <tmraz@redhat.com>
2009-05-26 22:23:19 +0200

    Fix version extraction from configure.ac

--- autogen.sh
+++ autogen.sh
@@ -4,5 +4,5 @@ aclocal -I m4
 autoheader
 automake -a
 autoconf
-VERSION=`sed '/AC_INIT/ !d; s/^.*, *//; s/).*$//' configure.ac`
+VERSION=$(sed '/AC_INIT/ !d; s/^[^[:digit:]]*//; s/[^[:digit:]]*$//' configure.ac)
 sed s/@PACKAGE_VERSION@/$VERSION/ fipscheck.spec.in > fipscheck.spec
