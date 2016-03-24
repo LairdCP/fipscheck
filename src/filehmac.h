fe97ce795f21d89d1889704ccb182ba0ade362b6
Tomas Mraz <tmraz@redhat.com>
2016-03-24 11:30:58 +0100

    Add missing copyright notices into sources.

--- src/filehmac.h
+++ src/filehmac.h
@@ -1,5 +1,6 @@
 /* filehmac.h */
 /*
+ * Copyright (C) 2008, 2009, 2010, 2013 Red Hat Inc. All rights reserved.
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  * 
