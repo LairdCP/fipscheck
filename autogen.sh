1bd17ca538faf668674b295e3e5b628a68689c15
Andrew Cagney <cagney@fedoraproject.org>
2017-02-23 15:11:30 +0100

    Properly initialize auto tools.

--- autogen.sh
+++ autogen.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 set -x
-aclocal -I m4
-autoheader
-libtoolize
-automake -a
-autoconf
+cd $(dirname $0)
+autoreconf --install
 VERSION=$(sed '/AC_INIT/ !d; s/^[^[:digit:]]*//; s/[^[:digit:]]*$//' configure.ac)
 sed s/@PACKAGE_VERSION@/$VERSION/ fipscheck.spec.in > fipscheck.spec
